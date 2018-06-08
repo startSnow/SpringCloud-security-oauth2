d74b3375a28fc4a8e361a653988bf2fabc96b5f8
chixue <334548450@qq.com>
2018-06-08 16:51:52 +0800

    提交

--- springcloud-oauth2-auth-server/src/main/java/org/springcloud/oauth2/auth/server/OAuth2Config.java
+++ springcloud-oauth2-auth-server/src/main/java/org/springcloud/oauth2/auth/server/OAuth2Config.java
@@ -1,28 +1,69 @@
 package org.springcloud.oauth2.auth.server;
-import javax.sql.DataSource;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.oauth2.common.DefaultOAuth2AccessToken;
+import org.springframework.security.oauth2.common.OAuth2AccessToken;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
-import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 @EnableAuthorizationServer
 public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
-
+	
 	@Autowired
 	private AuthenticationManager authenticationManager;
-	
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		endpoints
-		.authenticationManager(this.authenticationManager)					
-		.tokenStore(tokenStore());	
+		endpoints.authenticationManager(this.authenticationManager);
+		endpoints.accessTokenConverter(accessTokenConverter());
+		endpoints.tokenStore(tokenStore());
+	}
+	@Bean
+	public JwtAccessTokenConverter accessTokenConverter() {
+		JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter() {
+			/***
+			 * 重写增强token方法,用于自定义一些token返回的信息
+			 */
+			@Override
+			public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+				String userName = authentication.getUserAuthentication().getName();
+				User user = (User) authentication.getUserAuthentication().getPrincipal();// 与登录时候放进去的UserDetail实现类一直查看link{SecurityConfiguration}
+				/** 自定义一些token属性 ***/
+				final Map<String, Object> additionalInformation = new HashMap<>();
+				additionalInformation.put("userName", userName);
+				additionalInformation.put("roles", user.getAuthorities());
+				((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInformation);
+				OAuth2AccessToken enhancedToken = super.enhance(accessToken, authentication);
+				return enhancedToken;
+			}
+
+		};
+		accessTokenConverter.setSigningKey("123");// 测试用,资源服务使用相同的字符达到一个对称加密的效果,生产时候使用RSA非对称加密方式
+		return accessTokenConverter;
+	}
+
+	/**
+	 * token store
+	 * 
+	 * @param accessTokenConverter
+	 * @return
+	 */
+	@Bean
+	public TokenStore tokenStore() {
+		TokenStore tokenStore = new JwtTokenStore(accessTokenConverter());
+		return tokenStore;
 	}
 	
 	@Override
@@ -31,6 +72,7 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 		oauthServer
 			.tokenKeyAccess("permitAll()")
 			.checkTokenAccess("isAuthenticated()");
+		
 	}
 
 	@Override
@@ -64,12 +106,5 @@ public class OAuth2Config extends AuthorizationServerConfigurerAdapter {
 			.autoApprove(true);
 	}
 	
-	@Autowired
-	private DataSource dataSource;
-		
-	@Bean
-	public JdbcTokenStore tokenStore() {
-		return new JdbcTokenStore(dataSource);
-	}
 
 }
--- springcloud-resource-server/src/main/java/org/springcloud/resource/server/ResourceServerConfig.java
+++ springcloud-resource-server/src/main/java/org/springcloud/resource/server/ResourceServerConfig.java
@@ -1,7 +1,6 @@
 package org.springcloud.resource.server;
 import javax.sql.DataSource;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,7 +9,12 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.DefaultTokenServices;
+import org.springframework.security.oauth2.provider.token.ResourceServerTokenServices;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 /*
  * 配置授权资源路径
  */
@@ -40,10 +44,67 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 		resources
 			.resourceId(RESOURCE_ID)
 			.tokenStore(tokenStore());
+		resources.tokenServices(defaultTokenServices());
 	}
 
-	@Bean
+/*	@Bean
 	public JdbcTokenStore tokenStore() {
 		return new JdbcTokenStore(dataSource);
-	}
+	}*/
+	
+	// ===================================================以下代码与认证服务器一致=========================================
+		/**
+		 * token存储,这里使用jwt方式存储
+		 * 
+		 * @param accessTokenConverter
+		 * @return
+		 */
+		@Bean
+		public TokenStore tokenStore() {
+			TokenStore tokenStore = new JwtTokenStore(accessTokenConverter());
+			return tokenStore;
+		}
+
+		/**
+		 * Token转换器必须与认证服务一致
+		 * 
+		 * @return
+		 */
+		@Bean
+		public JwtAccessTokenConverter accessTokenConverter() {
+			JwtAccessTokenConverter accessTokenConverter = new JwtAccessTokenConverter() {
+//				/***
+//				 * 重写增强token方法,用于自定义一些token返回的信息
+//				 */
+//				@Override
+//				public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
+//					String userName = authentication.getUserAuthentication().getName();
+//					User user = (User) authentication.getUserAuthentication().getPrincipal();// 与登录时候放进去的UserDetail实现类一直查看link{SecurityConfiguration}
+//					/** 自定义一些token属性 ***/
+//					final Map<String, Object> additionalInformation = new HashMap<>();
+//					additionalInformation.put("userName", userName);
+//					additionalInformation.put("roles", user.getAuthorities());
+//					((DefaultOAuth2AccessToken) accessToken).setAdditionalInformation(additionalInformation);
+//					OAuth2AccessToken enhancedToken = super.enhance(accessToken, authentication);
+//					return enhancedToken;
+//				}
+
+			};
+			accessTokenConverter.setSigningKey("123");// 测试用,授权服务使用相同的字符达到一个对称加密的效果,生产时候使用RSA非对称加密方式
+			return accessTokenConverter;
+		}
+
+		/**
+		 * 创建一个默认的资源服务token
+		 * 
+		 * @return
+		 */
+		@Bean
+		public ResourceServerTokenServices defaultTokenServices() {
+			final DefaultTokenServices defaultTokenServices = new DefaultTokenServices();
+			defaultTokenServices.setTokenEnhancer(accessTokenConverter());
+			defaultTokenServices.setTokenStore(tokenStore());
+			return defaultTokenServices;
+		}
+		// ===================================================以上代码与认证服务器一致=========================================
 }
